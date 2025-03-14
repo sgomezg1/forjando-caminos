61cda6c8f9c73556a9c4f58347b40f7a9acfec9d
Sebastian Gomez <sebasgomez5892@gmail.com>
2025-03-14 01:09:38 -0500

    Primer commit

---
+++ wp-content/plugins/ninja-forms/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'saturday-drive/ninja-forms',
+        'pretty_version' => 'dev-b512436ee5b340987f8938bbcf60ac41c3cc8638',
+        'version' => 'dev-b512436ee5b340987f8938bbcf60ac41c3cc8638',
+        'reference' => 'b512436ee5b340987f8938bbcf60ac41c3cc8638',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'saturday-drive/ninja-forms' => array(
+            'pretty_version' => 'dev-b512436ee5b340987f8938bbcf60ac41c3cc8638',
+            'version' => 'dev-b512436ee5b340987f8938bbcf60ac41c3cc8638',
+            'reference' => 'b512436ee5b340987f8938bbcf60ac41c3cc8638',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ wp-content/themes/forjando-caminos/header.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * The header for Astra Theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?><!DOCTYPE html>
+<?php astra_html_before(); ?>
+<html <?php language_attributes(); ?>>
+<head>
+<?php astra_head_top(); ?>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<?php
+if ( apply_filters( 'astra_header_profile_gmpg_link', true ) ) {
+	?>
+	<link rel="profile" href="https://gmpg.org/xfn/11"> 
+	<?php
+}
+?>
+<?php wp_head(); ?>
+<?php astra_head_bottom(); ?>
+</head>
+
+<body <?php astra_schema_body(); ?> <?php body_class(); ?>>
+<?php astra_body_top(); ?>
+<?php wp_body_open(); ?>
+
+<a
+	class="skip-link screen-reader-text"
+	href="#content"
+	title="<?php echo esc_attr( astra_default_strings( 'string-header-skip-link', false ) ); ?>">
+		<?php echo esc_html( astra_default_strings( 'string-header-skip-link', false ) ); ?>
+</a>
+
+<div
+<?php
+	echo wp_kses_post(
+		astra_attr(
+			'site',
+			array(
+				'id'    => 'page',
+				'class' => 'hfeed site',
+			)
+		)
+	);
+	?>
+>
+	<?php
+	astra_header_before();
+
+	astra_header();
+
+	astra_header_after();
+
+	astra_content_before();
+	?>
+	<div id="content" class="site-content">
+		<div class="ast-container">
+		<?php astra_content_top(); ?>
